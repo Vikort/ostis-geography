90f4a6682c05832695ea514eaa48a9fa148f45c5
ZariaPie <pilotka_fitomorfy@mail.ru>
2022-03-04 17:29:53 +0300

    Changed mistake in module name

--- problem-solver/py/FindCateringOrganizationByWorkingHoursAgent/FindCateringOrganizationByWorkingHoursModule.cpp
+++ problem-solver/py/FindCateringOrganizationByWorkingHoursAgent/FindCateringOrganizationByWorkingHoursModule.cpp
@@ -4,12 +4,12 @@ SC_IMPLEMENT_MODULE(FindCateringOrganizationByWorkingHoursAgentModule)
 
 sc_result FindCateringOrganizationByWorkingHoursAgentModule::InitializeImpl()
 {
-  m_FindCateringOrganizationByWorkingHoursService.reset(new FFindCateringOrganizationByWorkingHoursAgentPythonService("FindCateringOrganizationByWorkingHoursAgent/FindCateringOrganizationByWorkingHoursModule.py"));
+  m_FindCateringOrganizationByWorkingHoursService.reset(new FindCateringOrganizationByWorkingHoursAgentPythonService("FindCateringOrganizationByWorkingHoursAgent/FindCateringOrganizationByWorkingHoursModule.py"));
   m_FindCateringOrganizationByWorkingHoursService->Run();
   return SC_RESULT_OK;
 }
 
-sc_result FFindCateringOrganizationByWorkingHoursAgentModule::ShutdownImpl()
+sc_result FindCateringOrganizationByWorkingHoursAgentModule::ShutdownImpl()
 {
   m_FindCateringOrganizationByWorkingHoursService->Stop();
   m_FindCateringOrganizationByWorkingHoursService.reset();
