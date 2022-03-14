22a500d518b490497f17691ed54685577fa13959
kolya <kolya.vladimirsky@gmail.com>
2022-03-14 12:21:32 +0300

    holy agent

--- problem-solver/py/services/HolyAgent/HolyAgent.py
+++ problem-solver/py/services/HolyAgent/HolyAgent.py
@@ -3,17 +3,32 @@ from sc import *
 
 
 class HolyAgent(ScAgent):
+    def __init__(self, module):
+        super().__init__(module)
+        self.ctx = module.ctx
+        self.keynodes = ScKeynodes(self.ctx)
+        self.main_node = None
 
     def RunImpl(self, evt: ScEventParams) -> ScResult:
         print("Holy agent started")
-        city = evt.other_addr
-        search_area = self.module.ctx.HelperResolveSystemIdtf("nrel_search_area", ScType.NodeConstNorole)
+        self.main_node = evt.other_addr
+        status = ScResult.Ok
+
+        if not self.module.ctx.HelperCheckEdge(
+                self.keynodes['action_holy_find'],
+                self.main_node,
+                ScType.EdgeAccessConstPosPerm,
+        ):
+            return
+
+        print("HolyAgent activated")
+        search_area = self.module.ctx.HelperResolveSystemIdtf("nrel_city", ScType.NodeConstNorole)
         answer = self.module.ctx.HelperResolveSystemIdtf("HolyAnswer", ScType.NodeConstClass)
         faith = self.module.ctx.HelperResolveSystemIdtf("concept_faith_place", ScType.NodeConstClass)
         if answer.IsValid():
             print("answer valid")
 
-        iterate_city_places = self.module.ctx.Iterator5(city, ScType.EdgeDCommonConst, ScType.NodeConst, ScType.EdgeAccessConstPosPerm, search_area)
+        iterate_city_places = self.module.ctx.Iterator5(self.main_node, ScType.EdgeDCommonConst, ScType.NodeConst, ScType.EdgeAccessConstPosPerm, search_area)
         while iterate_city_places.Next():
             print("iterating places")
             iterate_methods = self.module.ctx.Iterator3(faith, ScType.EdgeAccessConstPosPerm, iterate_city_places.Get(2))
